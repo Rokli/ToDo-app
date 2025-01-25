4078a546925bc405d3e154d4b3b1548cef38de27
Rokli <satalomaksimka12345678910@gmail.com>
2025-01-25 13:29:45 +0300

    ready modal window(front)

--- js/accordion.js
+++ js/accordion.js
@@ -1,3 +1,5 @@
+let dayTrue = "123";
+
 document.querySelectorAll('.accordion-header').forEach(header => {
     header.addEventListener('click', () => {
       const accordion = header.parentElement;
@@ -5,9 +7,24 @@ document.querySelectorAll('.accordion-header').forEach(header => {
     });
   });
 
-function AddEvent(day){
-    const button = document.getElementById("button-" + day);
-    const accordion = button.closest(".accordion");
-    const content = accordion.querySelector(".accordion-content");
-    content.innerHTML = "<p>Новая задача добавлена!</p>";
+function addEvent(){
+  const task = document.getElementById("accordion-modal-task").value;
+
+
+
+  const button = document.getElementById("button-" + dayTrue);
+  const accordion = button.closest(".accordion");
+  const content = accordion.querySelector(".accordion-content");
+  content.innerHTML = `<p>${task}</p>`;
+}
+
+function openModal(day) {
+  const modal = document.getElementById("accordion-modal");
+  dayTrue = day;
+  modal.style.display = "flex"; 
+}
+
+function closeModal(){
+  const modal = document.getElementById("accordion-modal");
+  modal.style.display = "none"; 
 }
